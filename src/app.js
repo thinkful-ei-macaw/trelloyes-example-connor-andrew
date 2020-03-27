35f75cf350e4f90de31ae36bb1c4894f1f2d8d21
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-27 15:36:05 -0500

    everything working

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const winston = require('winston');
 const uuid = require('uuid/v4');
-
-
 const { NODE_ENV } = require('./config');
+const cardRouter = require('./card/card-router');
+const listRouter = require('./list/list-router');
+const logger = require('./logger');
 
 const app = express();
 
@@ -15,29 +15,15 @@ const morganOption = NODE_ENV === 'production'
   ? 'tiny'
   : 'common';
 
-const logger = winston.createLogger({
-  level: 'info', 
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
-});
-
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
-
 // standard middleware
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
 
 
 // authentication middleware
 app.use(function validateBearerToken(req, res, next) {
+  console.log('test 1');
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
 
@@ -49,143 +35,11 @@ app.use(function validateBearerToken(req, res, next) {
   next();
 });
 
+app.use(cardRouter)
+app.use(listRouter)
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
-});
-
-// local data
-const cards = [{
-  id: 1,
-  title: 'Task One',
-  content: 'This is card one'
-}];
-const lists = [{
-  id: 1,
-  header: 'List One',
-  cardIds: [1]
-}];
-
-app.get('/card', (req, res) => {
-  res
-    .json(cards);
-});
-
-app.get('/list', (req, res) => {
-  res
-    .json(lists);
-});
-
-app.get('/card/:id', (req, res) => {
-  const { id } = req.params;
-  const card = cards.find(c => c.id == id);
-
-  // make sure we found a card
-  if (!card) {
-    logger.error(`Card with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Card Not Found');
-  }
-
-  res.json(card);
-});
-
-app.get('/list/:id', (req, res) => {
-  const { id } = req.params;
-  const list = lists.find(li => li.id == id);
-
-  // make sure we found a list
-  if (!list) {
-    logger.error(`List with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('List Not Found');
-  }
-
-  res.json(list);
-});
-
-app.post('/card', (req, res) => {
-  const { title, content } = req.body;
-
-  if (!title) {
-    logger.error(`Title is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-  
-  if (!content) {
-    logger.error(`Content is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-  
-  // get an id
-  const id = uuid();
-
-  const card = {
-    id,
-    title,
-    content
-  };
-
-  cards.push(card);
-
-  logger.info(`Card with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/card/${id}`)
-    .json(card);
-});
-
-app.post('/list', (req, res) => {
-  const { header, cardIds = [] } = req.body;
-
-  if (!header) {
-    logger.error(`Header is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-
-  // check card IDs
-  if (cardIds.length > 0) {
-    let valid = true;
-    cardIds.forEach(cid => {
-      const card = cards.find(c => c.id == cid);
-      if (!card) {
-        logger.error(`Card with id ${cid} not found in cards array.`);
-        valid = false;
-      }
-    });
-
-    if (!valid) {
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  }
-
-  // get an id
-  const id = uuid();
-
-  const list = {
-    id,
-    header,
-    cardIds
-  };
-
-  lists.push(list);
-
-  logger.info(`List with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/list/${id}`)
-    .json({id});
 });
 
 app.use((error, req, res, next) => { // eslint-disable-line no-unused-vars
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const { PORT } = require('./config');
 
 app.listen(PORT, () => {
-  console.log(`Server listening in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`);
+  console.log(` Server listening in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`);
 });
